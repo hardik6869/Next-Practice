d2a8e2a849e4ebbd1a8ce79cee15505d8e168568
hardik6869 <sorthiyahardik6869@gmail.com>
2022-08-01 12:47:01 +0530

    Navigate Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,17 @@
+import { useRouter } from "next/router";
+
 const ClientProjectPage = () => {
+  const router = useRouter();
+  const loadProjectHandler = () => {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "brad", clientprojectid: "projectb" },
+    });
+  };
   return (
     <div>
       <h1> The Project of Given Client. </h1>
+      <button onClick={loadProjectHandler}> Load Project </button>
     </div>
   );
 };
