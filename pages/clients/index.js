948a5e8071d61f5501c709f89c9a03beda9e7e38
hardik6869 <sorthiyahardik6869@gmail.com>
2022-08-01 11:38:57 +0530

    Navigate to Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+  const clients = [
+    { id: "max", name: "Maxi Maximilian" },
+    { id: "brad", name: "Brad Travercy" },
+    { id: "mastery", name: "Javascript Mastery" },
+  ];
   return (
     <div>
       <h1> The Clients Page </h1>
+      {clients.map((client) => (
+        <li key={client.id}>
+          <Link href={`/clients/${client.id}`}>{client.name}</Link>
+        </li>
+      ))}
     </div>
   );
 };
