2392c048e223d7b1cc22ef2e7ab1f3f369e0fdf1
hardik6869 <sorthiyahardik6869@gmail.com>
2022-08-01 10:51:58 +0530

    Nested Dynamic Routes & Paths

---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,9 @@
+const ClientProjectPage = () => {
+  return (
+    <div>
+      <h1> The Project of Given Client. </h1>
+    </div>
+  );
+};
+
+export default ClientProjectPage;
--- pages/portfolio/[projectId].js
+++ pages/portfolio/[projectId].js
@@ -2,8 +2,10 @@ import { useRouter } from "next/router";
 
 const PortfolioProjectPage = () => {
   const router = useRouter();
-  console.log(router.asPath);
+  console.log(router.pathname);
   console.log(router.query.projectId);
+
+  // Send a request to some backend serverto fetchthe piece of data with an id of router.query.projectId.
   return (
     <div>
       <h1>The PortfolioProject Page </h1>
