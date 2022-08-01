ffbb9a1aafffab9703db65e82394131810e7a507
hardik6869 <sorthiyahardik6869@gmail.com>
2022-08-01 10:31:12 +0530

    Extracting Dynamic Path Segment Data(Dyanamic Routes)

--- pages/portfolio/[projectId].js
+++ pages/portfolio/[projectId].js
@@ -1,4 +1,9 @@
+import { useRouter } from "next/router";
+
 const PortfolioProjectPage = () => {
+  const router = useRouter();
+  console.log(router.asPath);
+  console.log(router.query.projectId);
   return (
     <div>
       <h1>The PortfolioProject Page </h1>
