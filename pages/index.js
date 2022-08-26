cb54a5cb86bab5dc8c5cf6e25437ea8f15d9f154
hardik6869 <sorthiyahardik6869@gmail.com>
2022-08-26 12:14:51 +0530

    Parsing the incoming Request & Executing Server-Side Code

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import Head from "next/head";
+import { useRef } from "react";
 
 export default function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    enteredEmail = emailInputRef.current.value;
+    enteredFeedback = feedbackInputRef.current.value;
+  };
+
   return (
     <div>
       <Head>
@@ -8,6 +19,17 @@ export default function Home() {
       </Head>
       <div className="text-center">
         <h1> Api Routes Practice</h1>
+        <form onSubmit={submitFormHandler}>
+          <div>
+            <label htmlFor="email"> Your Email Address</label>
+            <input type="email" id="email" ref={emailInputRef} />
+          </div>
+          <div>
+            <label htmlFor="feedback"> Your Feedback</label>
+            <textarea id="feedback" rows={5} ref={feedbackInputRef}></textarea>
+          </div>
+          <button>Send Feedback</button>
+        </form>
       </div>
     </div>
   );
